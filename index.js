61baccea884cd3cd8783aab81aa391db88088c1d
Sharukh4129 <khansharukhappic12@gmail.com>
2024-01-02 16:12:39 +0530

    u

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 4400
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('hitesh')
+})
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login at chai or code </h1>')
+})
+app.get("/youtube", (req, res) => {
+    res.send('Chai or Code')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}!`)
+}) 
+////
